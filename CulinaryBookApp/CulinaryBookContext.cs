10bb27a79ecfddc8b8c58e5aa29109a9f24e63c0
jpraj <TUgit2020!>
2021-06-01 22:25:25 +0200

    all DbSet

--- CulinaryBookApp/CulinaryBookContext.cs
+++ CulinaryBookApp/CulinaryBookContext.cs
@@ -58,5 +58,10 @@ namespace CulinaryBookApp
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Step> Steps { get; set; }
+        public DbSet<IngredientsList> IngredientsLists { get; set; }
+        public DbSet<RecipesList> RecipesLists { get; set; }
+        public DbSet<StepsList> StepsLists { get; set; }
     }
 }
