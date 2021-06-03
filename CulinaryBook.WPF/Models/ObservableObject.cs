e694db4ffd50c602949cc5e2dc448c32e0cf7b6b
jpraj <TUgit2020!>
2021-06-03 16:26:42 +0200

    Observable object

---
+++ CulinaryBook.WPF/Models/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CulinaryBook.WPF.Annotations;
+
+namespace CulinaryBook.WPF.Models
+{
+    public class ObservableObject : INotifyPropertyChanged
+
+    {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    }
+}
