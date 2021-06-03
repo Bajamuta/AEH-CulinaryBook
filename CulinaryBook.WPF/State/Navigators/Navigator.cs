b288aae0f24264a49e33ea8c2f528c9146988378
jpraj <TUgit2020!>
2021-06-03 16:21:47 +0200

    working on current view model

--- CulinaryBook.WPF/State/Navigators/Navigator.cs
+++ CulinaryBook.WPF/State/Navigators/Navigator.cs
@@ -1,12 +1,32 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CulinaryBook.WPF.Annotations;
 using CulinaryBook.WPF.Commands;
 using CulinaryBook.WPF.ViewModels;
 
 namespace CulinaryBook.WPF.State.Navigators
 {
-    public class Navigator : INavigator
+    public class Navigator : INavigator, INotifyPropertyChanged
     {
-        public ViewModelBase CurrentViewModel { get; set; }
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
