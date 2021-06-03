b661af6066f36f0385e1799e0426031eff1033fb
jpraj <TUgit2020!>
2021-06-03 12:22:13 +0200

    first user controls

---
+++ CulinaryBook.WPF/Controls/NavigationBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CulinaryBook.WPF.Controls
+{
+    public partial class NavigationBar : UserControl
+    {
+        public NavigationBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
