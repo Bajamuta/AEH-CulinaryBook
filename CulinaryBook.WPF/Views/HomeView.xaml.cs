14ebc212359a102015641488b7caa1bb2a1e7bbf
jpraj <TUgit2020!>
2021-06-03 17:05:47 +0200

    Basic view pages

---
+++ CulinaryBook.WPF/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CulinaryBook.WPF.Views
+{
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
