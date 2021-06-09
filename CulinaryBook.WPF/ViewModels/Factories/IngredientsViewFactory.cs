0521bb12ca520ac70b1c9139ec96b9e5d6173d8a
jpraj <R4vW\fY+rO&R1i/l>
2021-06-09 14:50:35 +0200

    adding ingredient after only after logging in

--- CulinaryBook.WPF/ViewModels/Factories/IngredientsViewFactory.cs
+++ CulinaryBook.WPF/ViewModels/Factories/IngredientsViewFactory.cs
@@ -1,19 +1,23 @@
 ﻿using CulinaryBook.ConsoleApp.Services.IngredientServices;
+using CulinaryBook.WPF.State.Authenticators;
 
 namespace CulinaryBook.WPF.ViewModels.Factories
 {
     public class IngredientsViewFactory : IViewModelFactory<IngredientsViewModel>
     {
         private readonly IIngredientDataService _service;
+        private readonly IAuthenticator _authenticator;
 
-        public IngredientsViewFactory(IIngredientDataService service)
+        public IngredientsViewFactory(IIngredientDataService service,
+            IAuthenticator authenticator)
         {
             _service = service;
+            _authenticator = authenticator;
         }
 
         public IngredientsViewModel CreateViewModel()
         {
-            return new IngredientsViewModel(_service);
+            return new IngredientsViewModel(_service, _authenticator);
         }
     }
 }
--- CulinaryBook.WPF/ViewModels/IngredientsViewModel.cs
+++ CulinaryBook.WPF/ViewModels/IngredientsViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Input;
+using CulinaryBook.ConsoleApp;
 using CulinaryBook.ConsoleApp.Models;
 using CulinaryBook.ConsoleApp.Services.IngredientServices;
 using CulinaryBook.WPF.Commands;
 using CulinaryBook.WPF.Commands.Ingredients;
 using CulinaryBook.WPF.Models;
+using CulinaryBook.WPF.State.Authenticators;
 
 namespace CulinaryBook.WPF.ViewModels
 {
@@ -12,6 +14,7 @@ namespace CulinaryBook.WPF.ViewModels
     {
         private string _ingredientName;
         private string _ingredientJunit;
+        private Author _recipeAuthor;
         
         public string IngredientName
         {
@@ -32,12 +35,29 @@ namespace CulinaryBook.WPF.ViewModels
                 OnPropertyChanged(nameof(IngredientJunit));
             }
         }
+        
+        public Author RecipeAuthor
+        {
+            get => _recipeAuthor;
+            set
+            {
+                _recipeAuthor = value;
+                OnPropertyChanged(nameof(RecipeAuthor));
+                OnPropertyChanged(nameof(IsLogged));
+            }
+        }
+
+        public bool IsLogged => RecipeAuthor != null;
 
         public ICommand SearchIngredientCommand { get; }
         public ICommand AddIngredientCommand { get; }
 
-        public IngredientsViewModel(IIngredientDataService ingredientDataService)
+        public IngredientsViewModel(IIngredientDataService ingredientDataService, IAuthenticator authenticator)
         {
+            if (authenticator.CurrentUser != null)
+            {
+                RecipeAuthor = authenticator.CurrentUser; 
+            }
             SearchIngredientCommand = new SearchIngredientCommand(this, ingredientDataService);
             AddIngredientCommand = new AddIngredientCommand(this, ingredientDataService);
             // TODO get all ingredients
